01925d5b722aa8a2762445a9f6d60eefff012911
t-kusakabe <ttmac0704@gmail.com>
2021-08-27 14:51:35 +0900

    ひとまずexample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-const Top: React.FC = () => {
+import {useState} from 'react';
+import {useForm, SubmitHandler} from 'react-hook-form';
+
+type Inputs = {
+  title: string;
+  content: string;
+};
+
+const App: React.FC = () => {
+  const {register, handleSubmit, watch, formState: {errors}} = useForm<Inputs>();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <div>
-      Top Page.
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("title", {required: true, maxLength: 30})} />
+      <textarea {...register("content", {required: true, maxLength: 100})} />
+      {errors.title && <span>タイトル必須</span>}
+
+      <input type="submit" />
+    </form>
   );
 };
 
-export default Top;
+export default App;
